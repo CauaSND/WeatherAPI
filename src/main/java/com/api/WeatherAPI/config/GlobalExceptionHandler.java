4939bd99f3d7ff7267bba427ade5fbaa4cf72581
Cauã Silva <caua.sndias@gmail.com>
2026-06-02 13:32:36 -0300

    feat(exception): add handler for when required fields are null

--- src/main/java/com/api/WeatherAPI/config/GlobalExceptionHandler.java
+++ src/main/java/com/api/WeatherAPI/config/GlobalExceptionHandler.java
@@ -3,20 +3,27 @@ package com.api.WeatherAPI.config;
 import com.api.WeatherAPI.expection.LocationNotFoundException;
 import com.api.WeatherAPI.expection.WeatherApiException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(value = LocationNotFoundException.class)
-    public ResponseEntity<String> notFoundLocation(LocationNotFoundException e){
+    @ExceptionHandler (value = LocationNotFoundException.class)
+    public ResponseEntity<String> notFoundLocation (LocationNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
-    @ExceptionHandler(value = WeatherApiException.class)
-    public ResponseEntity<String> weatherApiException (WeatherApiException e){
+    @ExceptionHandler (value = WeatherApiException.class)
+    public ResponseEntity<String> weatherApiException (WeatherApiException e) {
         return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    @ExceptionHandler (MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException (MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatusCode.valueOf(422)).body(e.getMessage());
     }
 
 }
